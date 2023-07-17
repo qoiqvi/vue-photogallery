b3b1cd9df413b96ff35a961fb849541cd99cd4f2
qoiqvi <111002830+qoiqvi@users.noreply.github.com>
2023-07-17 22:35:42 +0500

    vue photogallery

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+	<v-container fluid>
+		<v-app-bar
+			color="deep-purple accent-4"
+			dark
+		>
+			<v-spacer />
+			<v-btn
+				outlined
+				class="ml-3"
+				>Главная</v-btn
+			>
+			<v-btn
+				outlined
+				class="ml-3"
+				>Фото</v-btn
+			>
+		</v-app-bar>
+	</v-container>
+</template>
+
+<script>
+export default {
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: "Navbar",
+}
+</script>
+
+<style scoped></style>
